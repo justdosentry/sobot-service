1858560945b456b971a615a392cacb753709a5f4
sentry <sentry@sentrys-iMac-Pro.local>
2019-07-13 16:50:11 +0800

    新提交

---
+++ src/test/java/com/iqianjin/sobotservice/SobotServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iqianjin.sobotservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SobotServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
